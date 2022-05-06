2779216030af6cdd4fd006d3ce46ad6037ae11b9
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2022-05-06 22:59:34 +0200

    Draft version: ready
    All pages created. Sample content added.

---
+++ scripts/carousel.js
@@ -0,0 +1,9 @@
+const arrowLeft = document.querySelector(".arrow-left-button");
+const arrowRight = document.querySelector(".arrow-right-button");
+
+const firstFourPosts = document.querySelectorAll(".page-one");
+const secondFourPosts = document.querySelectorAll(".page-two");
+
+
+/* arrowRight.style.opacity = "100%"; */
+/* firstFourPosts.style.display = "none"; */
---
+++ scripts/hamburger.js
@@ -0,0 +1,11 @@
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const menu = document.querySelector(".menu");
+
+
+/* if(menu.style.display = "none") {
+    hamburgerMenu.addEventListener("click", showMenu);
+    function showMenu() {
+    menu.style.display = "block";
+    }
+}
+ */
