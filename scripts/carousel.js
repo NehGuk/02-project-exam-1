5724337b526a5eadeee5d6716265683f639c8ffd
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2022-05-12 16:18:00 +0200

    Design changes

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,9 +1,18 @@
 const arrowLeft = document.querySelector(".arrow-left-button");
 const arrowRight = document.querySelector(".arrow-right-button");
-
 const firstFourPosts = document.querySelectorAll(".page-one");
 const secondFourPosts = document.querySelectorAll(".page-two");
+const first = document.getElementById(".page-one");
+const second = document.getElementById(".page-two");
 
 
-/* arrowRight.style.opacity = "100%"; */
-/* firstFourPosts.style.display = "none"; */
+arrowRight.style.display = "block";
+arrowLeft.style.display = "block";
+arrowLeft.style.opacity = "100%";
+arrowRight.style.opacity = "100%";
+
+/* firstFourPosts.style.display = "none";
+secondFourPosts.style.display = "none"; */
+
+/* first.style.display = "none";
+second.style.display = "none"; */
