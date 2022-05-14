3effaa4b6d08d3e34a59ccc57a7d56ceebff008f
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2022-05-14 21:44:24 +0200

    Post page: tags generated dynamically

--- scripts/api-calls/post.js
+++ scripts/api-calls/post.js
@@ -5,53 +5,65 @@ const mainGrid = document.querySelector(".main-grid");
 
 // GETTING THE POST CONTENT
 async function getPost() {
-    try {
-        const result = await fetch(urlPost);
-        const post = await result.json();
-        /* console.log(post.acf.title);
-        console.log(post.acf.content);
-        console.log(post.acf.author);
-        console.log(post.acf.date);
-        console.log(post.acf.topics[0].name);
-        console.log(post.acf.topics[1].name);
-        console.log(post.acf.topics[2].name); */
+  try {
+    const result = await fetch(urlPost);
+    const post = await result.json();
+    /* console.log(post.acf.title);
+    console.log(post.acf.content);
+    console.log(post.acf.author);
+    console.log(post.acf.date);
+    console.log(post.acf.topics[0].name);
+    console.log(post.acf.topics[1].name);
+    console.log(post.acf.topics[2].name); */
 
-        mainGrid.innerHTML = "";
-        
-        function createHTMLpost() {
-            mainGrid.innerHTML = `
-            <!-- HEADER -->
-            <header>
-              <div class="image">
-                <img class="image-post" src="${post.acf.image}">
-              </div>
-            </header>
-          
-            <!-- CONTENT -->
-            <div class="content">
-              <h2>${post.acf.title}</h2>
-              <p class="regular-text">${post.content.rendered}</p>
-                <hr>
-                <p><span class="author-name"><strong>${post.acf.author}</strong></span></p>
-                <br>
-                <p><span class="date">${post.acf.date}</span></p>
-                <br>
-                <div>
-                        <a class="tag-label" href="#">${post.acf.topics[0].name}</a>
-                        <a class="tag-label" href="#">${post.acf.topics[1].name}</a>
-                        <a class="tag-label" href="#">${post.acf.topics[2].name}</a>
-                </div>
-            </div>    
-            `;
-        }
-        createHTMLpost();
+    mainGrid.innerHTML = "";
+    
+    function createHTMLpost() {
+      mainGrid.innerHTML = `
+        <!-- HEADER -->
+        <header>
+          <div class="image">
+            <img class="image-post" src="${post.acf.image}">
+          </div>
+        </header>
+      
+        <!-- CONTENT -->
+        <div class="content">
+          <h2>${post.acf.title}</h2>
+          <p class="regular-text">${post.content.rendered}</p>
+            <hr>
+            <p><span class="author-name"><strong>${post.acf.author}</strong></span></p>
+            <br>
+            <p><span class="date">${post.acf.date}</span></p>
+            <br>
 
-
+          <!-- TAGS CONTAINER -->
+            <div class="tag-container">
+            </div>
+            </div>
+        `;  
     }
-        catch(error) {
+    createHTMLpost();
+
+    async function fetchPostTags() {
+      const resultTag = await fetch(urlPost);
+      const tags = await resultTag.json();
+      const tagContainer = document.querySelector(".tag-container");
+      // console.log(tags.acf.topics.length);    
+      // console.log("test");
+      
+      for(let t = 0; t < tags.acf.topics.length; t++) {
+        // console.log(tags.acf.topics[t].name);
+        tagContainer.innerHTML += `
+          <p class="tag-label">${tags.acf.topics[t].name}</p>
+        `;
+      }
+    }
+    fetchPostTags();
+    
+  }
+  catch(error) {
         console.log(`An error has occurred: ${error}`);
-    }
+  }
 }
 getPost();
-
-
