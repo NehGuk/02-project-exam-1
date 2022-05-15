437eb4a0e590e3ac283f14cab0a361ed4260a054
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2022-05-15 22:41:07 +0200

    Index: first steps with arrow right

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,18 +1,84 @@
-const arrowLeft = document.querySelector(".arrow-left-button");
-const arrowRight = document.querySelector(".arrow-right-button");
-const firstFourPosts = document.querySelectorAll(".page-one");
-const secondFourPosts = document.querySelectorAll(".page-two");
-const first = document.getElementById(".page-one");
-const second = document.getElementById(".page-two");
+const urlPosts = "https://gingaikidb.henrikugler.no/wp-json/wp/v2/posts?categories=12&acf_format=standard&_fields=id,content,date,acf&per_page=8";
+const carContainer = document.querySelector(".carousel-container");
 
 
-arrowRight.style.display = "block";
-arrowLeft.style.display = "block";
-arrowLeft.style.opacity = "100%";
-arrowRight.style.opacity = "100%";
 
-/* firstFourPosts.style.display = "none";
-secondFourPosts.style.display = "none"; */
 
-/* first.style.display = "none";
-second.style.display = "none"; */
+async function getCarouselPosts() {
+    const result = await fetch(urlPosts);
+    const carPosts = await result.json();
+
+    carContainer.innerHTML = `
+        <button class="arrow-left-button"><div class="grid-item arrow-left-container">
+            <img src="/assets/icons/arrow-back.png">
+        </div></button>
+        `;
+
+    for(let i = 0; i < 4; i++) {
+        carContainer.innerHTML += `        
+            <div class="grid-item page-one">
+                <a href="/post.html"><img class="card-image-small" src="${carPosts[i].acf.image}"></a>
+                
+                <a href="/post.html"><h3>${carPosts[i].acf.title}</h3></a>
+                <a href="/post.html"><h4 class="read-link">Read</h4></a>
+            </div>
+            `;
+    }
+    
+    carContainer.innerHTML += `
+        <button class="arrow-right-button"><div class="grid-item arrow-right-container">
+            <img src="/assets/icons/arrow-forward.png">
+        </div></button>
+        `;
+
+
+        function makeRightArrowWork() {
+            const arrowRightCont = document.querySelector(".arrow-right-container");
+            const arrowRight = document.querySelector(".arrow-right-button");
+            const arrowLeftCont = document.querySelector(".arrow-left-container");
+            const arrowLeft = document.querySelector(".arrow-left-button");
+            
+            arrowLeft.style.opacity = "0";
+            arrowRight.style.opacity = "1";
+        
+            arrowRight.onclick = function() {
+                carContainer.innerHTML = ``;
+        
+                carContainer.innerHTML += `
+                        <button class="arrow-left-button"><div class="grid-item arrow-left-container">
+                            <img src="/assets/icons/arrow-back.png">
+                        </div></button>
+                    `;
+        
+                for(let i = 4; i < 8; i++) {
+                    carContainer.innerHTML += `        
+                        <div class="grid-item page-one">
+                            <a href="/post.html"><img class="card-image-small" src="${carPosts[i].acf.image}"></a>
+                            <a href="/post.html"><h3>${carPosts[i].acf.title}</h3></a>
+                            <a href="/post.html"><h4 class="read-link">Read</h4></a>
+                        </div>
+                        `;
+                }   
+
+            }
+        }
+        makeRightArrowWork();        
+            
+    }
+    function notWorkingYet() {
+        const arrowLeft = document.querySelector(".arrow-left-button");
+        arrowLeft.onclick = function() {
+            console.log("sdsds");
+
+    }
+    notWorkingYet();
+    
+
+}
+getCarouselPosts();
+
+
+
+
+
+
