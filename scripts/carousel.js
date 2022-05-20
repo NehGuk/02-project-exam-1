b0dfd82757a0f06cf7e5cd80af736b3ef010152c
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2022-05-20 13:43:57 +0200

    Index: responsive on all screen sizes

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,91 +1,62 @@
 const urlPosts = "https://gingaikidb.henrikugler.no/wp-json/wp/v2/posts?categories=12&acf_format=standard&_fields=id,content,date,acf&per_page=8";
-const carContainer = document.querySelector(".carousel-container");
+const carSmallContainer = document.querySelector(".carousel-small-container");
+const arrowR = document.querySelector(".arrow-r");
+const arrowL = document.querySelector(".arrow-l");
 
-
+arrowL.innerHTML = ``;
 
 async function getCarouselPosts() {
     const result = await fetch(urlPosts);
-    const carPosts = await result.json();
-
-    carContainer.innerHTML = `
-        <button class="arrow-left-button"><div class="grid-item arrow-left-container">
-            <img src="/assets/icons/arrow-back.png">
-        </div></button>
-        `;
-
+    const latestPosts = await result.json();
+    carSmallContainer.innerHTML = ``;
     for(let i = 0; i < 4; i++) {
-        carContainer.innerHTML += `        
-            <div class="grid-item page-one">
-                <a href="/post.html?id=${carPosts[i].id}"><img class="card-image-small" src="${carPosts[i].acf.image}"></a>
-                <a href="/post.html?id=${carPosts[i].id}"><h3>${carPosts[i].acf.title}</h3></a>
-                <a href="/post.html?id=${carPosts[i].id}"><h4 class="read-link">Read</h4></a>
+        carSmallContainer.innerHTML += `        
+            <div class="card">
+                <a href="/post.html?id=${latestPosts[i].id}"><img class="card-image-small" src="${latestPosts[i].acf.image}"></a>
+                <a href="/post.html?id=${latestPosts[i].id}"><h3>${latestPosts[i].acf.title}</h3></a>
+                <a href="/post.html?id=${latestPosts[i].id}"><h4 class="read-link">Read</h4></a>
             </div>
             `;
     }
-    
-    carContainer.innerHTML += `
-        <button class="arrow-right-button"><div class="grid-item arrow-right-container">
-            <img src="/assets/icons/arrow-forward.png">
-        </div></button>
-        `;
-
-
-        function makeRightArrowWork() {
-            const arrowRightCont = document.querySelector(".arrow-right-container");
-            const arrowRight = document.querySelector(".arrow-right-button");
-            const arrowLeftCont = document.querySelector(".arrow-left-container");
-            const arrowLeft = document.querySelector(".arrow-left-button");
-            
-            arrowLeft.style.opacity = "0";
-            arrowRight.style.opacity = "1";
-        
-            arrowRight.onclick = function() {
-                carContainer.innerHTML = ``;
-        
-                carContainer.innerHTML += `
-                        <button class="arrow-left-button"><div class="grid-item arrow-left-container">
-                            <img src="/assets/icons/arrow-back.png">
-                        </div></button>
-                    `;
-        
-                for(let i = 4; i < 8; i++) {
-                    carContainer.innerHTML += `        
-                        <div class="grid-item page-one">
-                            <a href="/post.html?id=${carPosts[i].id}"><img class="card-image-small" src="${carPosts[i].acf.image}"></a>
-                            <a href="/post.html?id=${carPosts[i].id}"><h3>${carPosts[i].acf.title}</h3></a>
-                            <a href="/post.html?id=${carPosts[i].id}"><h4 class="read-link">Read</h4></a>
-                        </div>
-                        `;
-                }
-                
-                    carContainer.innerHTML += `
-                    <button class="arrow-right-button"><div class="grid-item arrow-right-container">
-                        <img src="/assets/icons/arrow-forward.png">
-                    </div></button>
-                    `;
-                    
-                            
-                
-            }
-            
-        }
-        makeRightArrowWork();        
-            
-    }
-    function notWorkingYet() {
-        const arrowLeft = document.querySelector(".arrow-left-button");
-        arrowLeft.onclick = function() {
-            console.log("sdsds");
-
-    }
-    notWorkingYet();
-    
-
 }
 getCarouselPosts();
 
+arrowR.onclick = async function getNextPosts() {
+    const result = await fetch(urlPosts);
+    const latestPosts = await result.json();
+    carSmallContainer.innerHTML = ``;
+    for(let i = 4; i < 8; i++) {
+        carSmallContainer.innerHTML += `        
+            <div class="card">
+                <a href="/post.html?id=${latestPosts[i].id}"><img class="card-image-small" src="${latestPosts[i].acf.image}"></a>
+                <a href="/post.html?id=${latestPosts[i].id}"><h3>${latestPosts[i].acf.title}</h3></a>
+                <a href="/post.html?id=${latestPosts[i].id}"><h4 class="read-link">Read</h4></a>
+            </div>
+            `;
+    }
+    arrowR.innerHTML = ``;
+    arrowL.innerHTML = `<button><img src="/assets/icons/arrow-back.png"></button>`;
+    console.log("Arrow right clicked");
+}
 
+arrowL.onclick = async function getPreviousPosts() {
+    const result = await fetch(urlPosts);
+    const latestPosts = await result.json();
+    carSmallContainer.innerHTML = ``;
+    for(let i = 0; i < 4; i++) {
+        carSmallContainer.innerHTML += `        
+            <div class="card">
+                <a href="/post.html?id=${latestPosts[i].id}"><img class="card-image-small" src="${latestPosts[i].acf.image}"></a>
+                <a href="/post.html?id=${latestPosts[i].id}"><h3>${latestPosts[i].acf.title}</h3></a>
+                <a href="/post.html?id=${latestPosts[i].id}"><h4 class="read-link">Read</h4></a>
+            </div>
+            `;
+    }
+    arrowR.innerHTML = `<button><img src="/assets/icons/arrow-forward.png"></button>`;
+    arrowL.innerHTML = ``;
+    
+    console.log("Arrow left clicked");
 
-
+}
 
 
